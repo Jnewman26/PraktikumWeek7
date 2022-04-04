5627afb4cfbacb14925e96e228abb85997958fb6
James Newman <jnewman01@student.ciputra.ac.id>
2022-04-04 20:04:15 +0700

    week7

--- PraktikumWeek7/Form1.Designer.cs
+++ PraktikumWeek7/Form1.Designer.cs
@@ -28,13 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxMasukan = new System.Windows.Forms.TextBox();
+            this.textBoxMenjadi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonKonversi = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Masukan Kalimat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Masukan Huruf";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(241, 43);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(352, 31);
+            this.textBoxInput.TabIndex = 2;
+            // 
+            // textBoxMasukan
+            // 
+            this.textBoxMasukan.Location = new System.Drawing.Point(241, 109);
+            this.textBoxMasukan.Name = "textBoxMasukan";
+            this.textBoxMasukan.Size = new System.Drawing.Size(94, 31);
+            this.textBoxMasukan.TabIndex = 3;
+            // 
+            // textBoxMenjadi
+            // 
+            this.textBoxMenjadi.Location = new System.Drawing.Point(499, 109);
+            this.textBoxMenjadi.Name = "textBoxMenjadi";
+            this.textBoxMenjadi.Size = new System.Drawing.Size(94, 31);
+            this.textBoxMenjadi.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Menjadi";
+            // 
+            // buttonKonversi
+            // 
+            this.buttonKonversi.Location = new System.Drawing.Point(241, 197);
+            this.buttonKonversi.Name = "buttonKonversi";
+            this.buttonKonversi.Size = new System.Drawing.Size(352, 38);
+            this.buttonKonversi.TabIndex = 6;
+            this.buttonKonversi.Text = "Konversi!";
+            this.buttonKonversi.UseVisualStyleBackColor = true;
+            this.buttonKonversi.Click += new System.EventHandler(this.buttonKonversi_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Hasil :";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(241, 303);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 25);
+            this.labelOutput.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonKonversi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMenjadi);
+            this.Controls.Add(this.textBoxMasukan);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxMasukan;
+        private System.Windows.Forms.TextBox textBoxMenjadi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonKonversi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelOutput;
     }
 }
 
--- PraktikumWeek7/Form1.cs
+++ PraktikumWeek7/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PraktikumWeek7
@@ -15,6 +8,35 @@ namespace PraktikumWeek7
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonKonversi_Click(object sender, EventArgs e)
+        {
+            string simpanKalimat = textBoxInput.Text.ToUpper();
+            char[] charArr = simpanKalimat.ToCharArray();
+            char sebelum = Convert.ToChar(textBoxMasukan.Text.ToUpper());
+            char sesudah = Convert.ToChar(textBoxMenjadi.Text.ToUpper());
+
+            int temp_sebelum = Convert.ToInt32(sebelum);
+            int temp_sesudah = Convert.ToInt32(sesudah);
+            int selisih = Math.Abs(temp_sesudah - temp_sebelum);
+
+            string output = "";
+
+            foreach (char c in charArr)
+            {
+                int temp = Convert.ToInt32(c) + selisih;
+                if (temp == 115)
+                {
+                    temp = 65;
+                }
+                else if (temp > 90)
+                {
+                    temp = temp - 26;
+                }
+                output = output + Convert.ToChar(temp).ToString();
+            }
+            labelOutput.Text = output.ToString();
         }
     }
 }
